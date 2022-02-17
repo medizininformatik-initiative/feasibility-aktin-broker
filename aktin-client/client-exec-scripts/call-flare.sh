2801d256f03f5974e38d36ac1562917ca79a8c1f
Julian Gründner <juliangruendner@googlemail.com>
2022-02-17 14:10:40 +0100

    update to new flare version and update cql execution script

--- aktin-client/client-exec-scripts/call-flare.sh
+++ aktin-client/client-exec-scripts/call-flare.sh
@@ -1,13 +1,17 @@
-#/bin/bash 
+#!/bin/sh 
 
 FLARE_BASE_URL=${FLARE_BASE_URL:-"http://flare:8080"}
 CLIENT_OBFUSCATE=${CLIENT_OBFUSCATE:-true}
 
 QUERY_INPUT=`cat`
 
+echo "##### INCOMING REQUEST at $(date) #####" >> aktin-requests.log
+echo "----BEGIN REQUEST----" >> aktin-requests.log
+echo $QUERY_INPUT >> aktin-requests.log
+echo "----END REQUEST----" >> aktin-requests.log
+
 RESP=$(curl --location --request POST "$FLARE_BASE_URL/query/execute" \
---header 'Accept-Encoding: CSQ' \
---header 'Content-Type: application/json' \
+--header 'Content-Type: application/sq+json' \
 --data-raw "$QUERY_INPUT")
 
 if [ $CLIENT_OBFUSCATE = true ]; then
@@ -15,5 +19,9 @@ if [ $CLIENT_OBFUSCATE = true ]; then
     RESP=$(($RESP - ($RESP % 10) + 10))
   fi
 fi
+
+echo "----BEGIN RESPONSE----" >> aktin-requests.log
+echo $RESP >> aktin-requests.log
+echo "----END RESPONSE----" >> aktin-requests.log
 
 printf "$RESP"
