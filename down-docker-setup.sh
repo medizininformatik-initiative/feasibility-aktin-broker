bd0dabeb0aca5a67834dd907c48a96aef07bc4d5
Julian Gründner <juliangruendner@googlemail.com>
2022-09-08 17:26:13 +0200

    add bash to image and change scripts to bash

--- down-docker-setup.sh
+++ down-docker-setup.sh
@@ -3,7 +3,7 @@
 export COMPOSE_PROJECT=feasibility-deploy
 
 cd aktin-broker
-docker-compose -p $COMPOSE_PROJECT up -d
+docker-compose -p $COMPOSE_PROJECT down
 sleep 10
 cd ../aktin-client
-docker-compose -p $COMPOSE_PROJECT up -d
+docker-compose -p $COMPOSE_PROJECT down
